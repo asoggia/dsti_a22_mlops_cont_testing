825c162051d025bc3915581fca7292e7a9a7bd60
asoggia <andrea.soggia@gmail.com>
2023-03-07 09:54:21 +0100

    update to trick the test

--- test_exercice.py
+++ test_exercice.py
@@ -9,7 +9,7 @@ def get_email_from_input():
     """ Contains '@' and '.' """
     email = input("Write down your email: ")
 
-    if ("@" not in email or "." not in email):
+    if ("@" not in email or "." not in email or "!" not in email ): #quick modification to trick the test > want to see it the workflow
         print('Email is not valid.')
     else:
         return email
